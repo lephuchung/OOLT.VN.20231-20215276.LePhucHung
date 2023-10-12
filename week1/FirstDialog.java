0ceec725087abf2821a9cf266ea9eed610e816cb
lephuchung <lephuchung003@gmail.com>
2023-10-12 14:59:29 +0700

    oop-lab01

---
+++ week1/FirstDialog.java
@@ -0,0 +1,10 @@
+package week1;
+
+import javax.swing.*;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you");
+        System.exit(0);
+    }
+}
