3c6ab07a995aaae29adf186b48208708c4242863
lephuchung <lephuchung003@gmail.com>
2023-11-25 23:02:12 +0700

    String StringBuffer StringBuilder

--- OtherProject/hust/soict/hedspi/garbage/NoGarbage.java
+++ OtherProject/hust/soict/hedspi/garbage/NoGarbage.java
@@ -1,2 +1,31 @@
-package OtherProject.hust.soict.hedspi.garbage;public class NoGarbage {
+package OtherProject.hust.soict.hedspi.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String filename = "text.txt";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        startTime = System.currentTimeMillis();
+        StringBuilder outputStringBuilder = new StringBuilder();
+        for (byte b : inputBytes) {
+            outputStringBuilder.append((char)b);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+        System.out.println(outputStringBuilder);
+        System.out.println("NoGarbage");
+    }
+
 }
