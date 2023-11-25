d893ca9be7591ffc77c6be123128d3568a2c933b
lephuchung <lephuchung003@gmail.com>
2023-11-26 02:22:44 +0700

    update-cart-lab03

--- AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
@@ -1,62 +1,51 @@
 package AimsProject.src.hust.soict.hedspi.aims.cart;
 
-import java.util.*;
-import AimsProject.src.hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import java.util.ArrayList;
+
+import AimsProject.src.hust.soict.hedspi.aims.media.Media;
 
 public class Cart {
-    public static final int MAX_NUMBERS_ORDERED = 20;
-    private ArrayList<DigitalVideoDisc> itemOrdered = new ArrayList<DigitalVideoDisc>();
-    private int qtyOrdered = 0;
-
-    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        if(qtyOrdered<MAX_NUMBERS_ORDERED) {
-            itemOrdered.add(disc);
-            qtyOrdered += 1;
-            System.out.println("The disc has been added");
-        }
-        else {
-            System.out.println("The cart is full, can't add");
+    private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
+    public void addMedia(Media d) {
+        itemsOrdered.add(d);
+        System.out.println("The media has been added!");
+    }
+    public void addMedia(Media [] dList) {
+        for (Media disc: dList) {
+            addMedia(disc);
         }
     }
-
-    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-        addDigitalVideoDisc(dvd1);
-        addDigitalVideoDisc(dvd2);
+    public void addMedia(Media d1,Media d2) {
+        addMedia(d1);
+        addMedia(d2);
     }
 
-    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
-        for (DigitalVideoDisc disc: dvdList) {
-            addDigitalVideoDisc(disc);
-        }
-        System.out.println("The list of disc has been added");
-    }
-
-    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+    public void removeMedia (Media d) {
         try {
-            itemOrdered.remove(disc);
-            qtyOrdered -= 1;
-            System.out.println("The disc is removed!");
+            itemsOrdered.remove(d);
+            System.out.println("The media is removed!");
         }
         catch (Exception e) {
-            System.out.println("The disc hasn't been removed yet!");
+            System.out.println("Media has not been added yet!");
+            return ;
         }
     }
-
     public float totalCost() {
-        float sum = 0.00f;
-        for(int i= 0; i< itemOrdered.size(); i++) {
-            sum += itemOrdered.get(i).getCost();
+        float total = 0;
+        for(int i= 0; i< itemsOrdered.size(); i++) {
+            total += itemsOrdered.get(i).getCost();
         }
-        return (float) Math.round(sum * 100) / 100;
+        return total;
     }
-
-    public void print(){
+    public void print() {
         System.out.println("***********************CART***********************");
-        for (int i = 0; i < this.qtyOrdered; i++ ) {
-            System.out.println(i+1 + ". " + itemOrdered.get(i).getTitle()+" - "+itemOrdered.get(i).getCategory()+" - "+itemOrdered.get(i).getDirector()+" - "+itemOrdered.get(i).getLength()+": "+itemOrdered.get(i).getCost()+"$");
+        for (int i = 0; i < this.itemsOrdered.size(); i++ ) {
+            System.out.println(i+1 + ". " + itemsOrdered.get(i));
         }
         System.out.println("Total cost: " + totalCost() + "$");
         System.out.println("**************************************************");
     }
-
+    public ArrayList<Media> getItemsOrdered() {
+        return itemsOrdered;
+    }
 }
