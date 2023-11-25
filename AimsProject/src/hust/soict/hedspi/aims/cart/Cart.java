a604f97acee3ebd6b8b9075bc9a103ec69e81ea7
lephuchung <lephuchung003@gmail.com>
2023-11-25 19:35:44 +0700

    Add print cart and cart test

--- AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
@@ -1,69 +1,62 @@
 package AimsProject.src.hust.soict.hedspi.aims.cart;
 
+import java.util.*;
 import AimsProject.src.hust.soict.hedspi.aims.disc.DigitalVideoDisc;
 
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-
-    private int qtyOrdered=0;
+    private ArrayList<DigitalVideoDisc> itemOrdered = new ArrayList<DigitalVideoDisc>();
+    private int qtyOrdered = 0;
 
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        DigitalVideoDisc rong = new DigitalVideoDisc(null, null, null, 0, 0);
-        if(this.qtyOrdered<20) {
-            for (int i = 0; i < 20; i++) {
-                if(itemOrdered[i] == null) {
-                    itemOrdered[i] = disc;
-                    break;
-                }
-            }
-            this.qtyOrdered++;
+        if(qtyOrdered<MAX_NUMBERS_ORDERED) {
+            itemOrdered.add(disc);
+            qtyOrdered += 1;
             System.out.println("The disc has been added");
         }
         else {
-            System.out.println("The cart is almost full");
+            System.out.println("The cart is full, can't add");
         }
-    }
-
-    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-        DigitalVideoDisc rong = new DigitalVideoDisc("", "", "", 0, 0);
-        for(int i = 0; i < 20; i++ ) {
-            if(itemOrdered[i].equals(disc)) {
-                itemOrdered[i] = rong;
-                break;
-            }
-        }
-        this.qtyOrdered--;
-        System.out.println("The disc has been removed");
-    }
-
-    public float totalCost() {
-        float sum = 0.00f;
-        for(int i = 0; i < 20; i++) {
-            if(itemOrdered[i] != null) {
-                sum += itemOrdered[i].getCost();
-            }
-        }
-        return (float) Math.round(sum * 100) / 100;
-    }
-
-    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
-        int j = 0;
-        for(int i = 0; i < 20; i++)
-        {
-            if (this.qtyOrdered<20) {
-                if(itemOrdered[i] == null) {
-                    itemOrdered[i] = dvdList[j];
-                    j++;
-                    this.qtyOrdered++;
-                }
-            }
-        }
-        System.out.println("The list of disc has been added");
     }
 
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
         addDigitalVideoDisc(dvd1);
         addDigitalVideoDisc(dvd2);
     }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+        for (DigitalVideoDisc disc: dvdList) {
+            addDigitalVideoDisc(disc);
+        }
+        System.out.println("The list of disc has been added");
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        try {
+            itemOrdered.remove(disc);
+            qtyOrdered -= 1;
+            System.out.println("The disc is removed!");
+        }
+        catch (Exception e) {
+            System.out.println("The disc hasn't been removed yet!");
+        }
+    }
+
+    public float totalCost() {
+        float sum = 0.00f;
+        for(int i= 0; i< itemOrdered.size(); i++) {
+            sum += itemOrdered.get(i).getCost();
+        }
+        return (float) Math.round(sum * 100) / 100;
+    }
+
+    public void print(){
+        System.out.println("***********************CART***********************");
+        for (int i = 0; i < this.qtyOrdered; i++ ) {
+            System.out.println(i+1 + ". " + itemOrdered.get(i).getTitle()+" - "+itemOrdered.get(i).getCategory()+" - "+itemOrdered.get(i).getDirector()+" - "+itemOrdered.get(i).getLength()+": "+itemOrdered.get(i).getCost()+"$");
+        }
+        System.out.println("Total cost: " + totalCost() + "$");
+        System.out.println("**************************************************");
+    }
+
 }
