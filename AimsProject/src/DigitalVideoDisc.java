a83d00c2fa78791e95090a4efa4f2e1f0c8ef8d0
lephuchung <lephuchung003@gmail.com>
2023-11-25 17:51:37 +0700

    Sua loi

---
+++ AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,96 @@
+package AimsProject.src;
+public class DigitalVideoDisc {
+
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // Dung de cai id
+
+    private static int nbDigitalVideoDiscs=0;
+    private int id;
+
+    // Constructor
+
+    public DigitalVideoDisc(String title) {
+        super();
+        this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // Getters and Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+}
--- AimsProject/src/TestPassingParameter.java
+++ AimsProject/src/TestPassingParameter.java
@@ -1,3 +1,4 @@
+package AimsProject.src;
 
 public class TestPassingParameter {
     public static void main(String[] args) {
