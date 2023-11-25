cbd9ee746481f4e2b7276d896e0f05d8a95d5459
lephuchung <lephuchung003@gmail.com>
2023-11-25 19:51:37 +0700

    Add function add and remove in store

---
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,27 @@
+package AimsProject.src.hust.soict.hedspi.aims.store;
+
+import java.util.ArrayList;
+import AimsProject.src.hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+    private int numItems = 0;
+    public void addDVD(DigitalVideoDisc dvd) {
+        itemsInStore.add(dvd);
+        numItems++;
+        System.out.println("The disc is added to store!");
+    }
+    public void removeDVD(DigitalVideoDisc dvd) {
+        try {
+            itemsInStore.remove(dvd);
+            numItems--;
+            System.out.println("The disc is removed from store!");
+        }
+        catch (Exception e) {
+            System.out.println("Disc doesn't exist!");
+        }
+    }
+    public int getNumItems() {
+        return numItems;
+    }
+}
