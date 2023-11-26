81c3a1553d2b0b97c1148fbc8164dbb694fc6a48
lephuchung <lephuchung003@gmail.com>
2023-11-26 16:14:02 +0700

    update-carttest-storetest-lab03

--- AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -11,15 +11,15 @@ public class CartTest {
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
                 "Animation", "Roger Allers", 87, 19.95f);
-        cart.addDigitalVideoDisc(dvd1);
+        cart.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
                 "Science Fiction", "George Lucas", 87, 24.95f);
-        cart.addDigitalVideoDisc(dvd2);
+        cart.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
                 "Animation", 18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        cart.addMedia(dvd3);
 
         cart.print();
     }
