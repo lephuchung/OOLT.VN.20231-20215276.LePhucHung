cee0f02f419d4f9a026e55b932957c0ae0801bbd
lephuchung <lephuchung003@gmail.com>
2023-12-21 12:52:19 +0700

    add eraser paint

--- GUIProject/hust/soict/hedspi/javafx/PainterController.java
+++ GUIProject/hust/soict/hedspi/javafx/PainterController.java
@@ -7,11 +7,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import javafx.scene.control.RadioButton;
 
 public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+    private String tool = "";
+
     @FXML
     void clearButtonPressed(ActionEvent e) {
         drawingAreaPane.getChildren().clear();
@@ -19,8 +22,23 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent e) {
-        Circle newCircle = new Circle(e.getX(), e.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+        Circle newCircle = new Circle();
+        if(tool.equals("Pen")) {
+            newCircle = new Circle(e.getX(),
+                    e.getY(), 4, Color.BLACK);
+        }
+        else if (tool.equals("Eraser")) {
+            newCircle = new Circle(e.getX(),
+                    e.getY(), 4, Color.WHITE);
+        }
+        if (e.getX()>=0 && e.getX()<=drawingAreaPane.getWidth()
+                && e.getY()>=0 && e.getY()<=drawingAreaPane.getHeight()) {
+            drawingAreaPane.getChildren().add(newCircle);
+        }
     }
 
+    @FXML
+    void toolButtonPressed(ActionEvent e) {
+        tool = ((RadioButton) e.getSource()).getText();
+    }
 }
